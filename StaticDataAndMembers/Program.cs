bd2f295a943735098acde76416f3dcd4a217c61b
Максим <tomich7372@gmail.com>
2018-02-03 14:21:59 +0700

    eeee

---
+++ StaticDataAndMembers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Static Data *****\n");
+            // Создать счет
+            SavingsAccount s1 = new SavingsAccount(50);
+            // Вывести текущую процентную ставку
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+
+            // Попытка изменения проц ставки через свойство
+            SavingsAccount.SetInterestRate(0.04);
+            //Создание второго счета
+            SavingsAccount s2 = new SavingsAccount(100);
+
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    // Простой класс депозиторного счета
+    class SavingsAccount
+    {
+        // Данные уровня экземпляра
+        public double currBalance;
+        // Статический элемент данных
+        public static double currInterestRate;
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+ 
+        // Статические члены для установки/получения процентной ставки
+        public static void SetInterestRate(double newRate)
+        {
+            currInterestRate = newRate;
+        }
+
+        public static double GetInterestRate()
+        {
+            return currInterestRate;
+        }
+
+        public static double GetInterestRate(double newRate)
+        {
+            return currInterestRate;
+        }
+
+        // Статический конструктор
+        static SavingsAccount()
+        {
+            Console.WriteLine("In static ctor!");
+            currInterestRate = 0.12;
+        }
+    }
+}
